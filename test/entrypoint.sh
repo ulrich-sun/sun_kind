e446ed1f3954d76a3182e8d48f8cc1a5c18ecb12
ulrich-sun <ulrichnoumsi98@gmail.com>
2025-09-20 16:47:25 +0100

    k8s update 10

--- test/entrypoint.sh
+++ test/entrypoint.sh
@@ -2,6 +2,6 @@
 mount --make-shared /
 /usr/local/bin/containerd &
 sleep 5
-/usr/bin/dockerd &
-sleep 5
+# /usr/bin/dockerd &
+# sleep 5
 /usr/bin/kubelet --config=/etc/kubernetes/kubelet-config.yaml --kubeconfig=/etc/kubernetes/kubelet.conf
