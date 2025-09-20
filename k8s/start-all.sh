6e5ae2deb1a62c2c09500ca3a5b3f5051997ac5a
ulrich-sun <ulrichnoumsi98@gmail.com>
2025-09-20 15:18:40 +0100

    k8s update 10

--- k8s/start-all.sh
+++ k8s/start-all.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 mount --make-shared /
-/usr/bin/containerd &
+echo "Génération du fichier de configuration containerd..."
+mkdir -p /etc/containerd
+/usr/bin/containerd config default > /etc/containerd/config.toml
+/usr/bin/containerd >> /proc/1/fd/1 2>&1 &
 sleep 5
-/usr/bin/dockerd &
+/usr/bin/dockerd >> /proc/1/fd/1 2>&1 &
 sleep 5
 /usr/bin/kubelet --config=/etc/kubernetes/kubelet-config.yaml --kubeconfig=/etc/kubernetes/kubelet.conf --authentication-token-webhook=false --authorization-mode=AlwaysAllow
